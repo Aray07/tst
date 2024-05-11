956d641e2dbab514fa3598f40c3a53bc5e401d12
Aray07 <153151873+Aray07@users.noreply.github.com>
2024-05-12 03:01:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,14 +16,17 @@ waktu = int(input("Set Waktu Kirim Pesan (dalam detik): "))
 os.system('cls' if os.name == 'nt' else 'clear')
 
 with open("pesan.txt", "r") as f:
-    words = f.readlines()
+    messages = f.read().splitlines()
 
 with open("token.txt", "r") as f:
     authorization = f.readline().strip()
 
 while True:
+    # Gabungkan pesan menjadi satu pengiriman jika lebih dari satu baris
+    message = '\n'.join(random.choices(messages, k=random.randint(1, 5)))
+
     payload = {
-        'content': random.choice(words).strip()
+        'content': message
     }
 
     headers = {
@@ -32,6 +35,6 @@ while True:
 
     r = requests.post(f"https://discord.com/api/v9/channels/{channel_id}/messages", data=payload, headers=headers)
     print(Fore.WHITE + "Sent message to channel " + channel_id + ": ")
-    print(Fore.YELLOW + payload['content'])
+    print(Fore.YELLOW + message)
 
     time.sleep(waktu)
