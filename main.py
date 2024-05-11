eb442e64b0b106edb74675074e0fe910448617cd
Aray07 <153151873+Aray07@users.noreply.github.com>
2024-05-12 02:18:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,13 +10,7 @@ script = "Auto Post Discord"
 print("Script: " + script)
 print("===========================================\n")
 
-channel_ids = []
-while True:
-    channel_id = input("Masukkan ID channel (tekan Enter untuk selesai): ")
-    if not channel_id:
-        break
-    channel_ids.append(channel_id)
-
+channel_id = input("Masukkan ID channel: ")
 waktu = int(input("Set Waktu Kirim Pesan (dalam detik): "))
 
 os.system('cls' if os.name == 'nt' else 'clear')
@@ -28,19 +22,16 @@ with open("token.txt", "r") as f:
     authorization = f.readline().strip()
 
 while True:
-    for channel_id in channel_ids:
-        channel_id = channel_id.strip()
+    payload = {
+        'content': random.choice(words).strip()
+    }
 
-        payload = {
-            'content': random.choice(words).strip()
-        }
+    headers = {
+        'Authorization': authorization
+    }
 
-        headers = {
-            'Authorization': authorization
-        }
+    r = requests.post(f"https://discord.com/api/v9/channels/{channel_id}/messages", data=payload, headers=headers)
+    print(Fore.WHITE + "Sent message to channel " + channel_id + ": ")
+    print(Fore.YELLOW + payload['content'])
 
-        r = requests.post(f"https://discord.com/api/v9/channels/{channel_id}/messages", data=payload, headers=headers)
-        print(Fore.WHITE + "Sent message to channel " + channel_id + ": ")
-        print(Fore.YELLOW + payload['content'])
-
-        time.sleep(waktu)
+    time.sleep(waktu)
