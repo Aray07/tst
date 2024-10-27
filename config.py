ef7e5eb735319c7f971985df54714c34376e082f
Aray07 <153151873+Aray07@users.noreply.github.com>
2024-10-28 06:47:15 +0700

    Update and rename install.sh to config.py

---
+++ config.py
@@ -0,0 +1,28 @@
+# config.py
+
+   # Channel IDs
+   channel_ids = {
+       'channel_1': 'GANTI_ID',
+       'channel_2': 'GANTI_ID',
+       'channel_3': 'GANTI_ID'
+   }
+
+   # Messages
+   messages = {
+       channel_ids['channel_1']: "ISI_DISINI",
+       channel_ids['channel_2']: "ISI_DISINI",
+       channel_ids['channel_3']: "ISI_DISINI"
+   }
+
+   # Discord Bot Token
+   token = 'Token_Discord_LU'
+
+   # Webhook URL
+   webhook_url = "WEBHOOK_URL_LU"
+
+   # Delays in minutes
+   delays = {
+       'channel_1': (60, 70),
+       'channel_2': (10, 20),
+       'channel_3': (60, 70)
+   }
